febfbbcc9e8e4a23681a231f8c298698bb19189c
Trevor Bisson <4trevorbisson@gmail.com>
2024-03-18 21:18:04 -0500

    reorganized files for future additions

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Inventory from './components/inventory.js';
+import Inventory from './components/inventoryComponents/inventory.js';
 
 function App() {
   const products=[
