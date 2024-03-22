02bf8624c3e02d6a7b50c07b53190ca588315e7c
Trevor Bisson <4trevorbisson@gmail.com>
2024-03-21 20:36:28 -0500

    added mockup for inventory, item, product forms

---
+++ src/components/AddComponents/AddIngredientForm.js
@@ -0,0 +1,23 @@
+import React, {Component} from "react";
+import { useState } from "react";
+
+export default class AddIngredientForm extends Component {
+
+    const [pname,setPName] = useState("");
+
+    function pNameChangeHandler=(e)=>{
+        setInventoryItem{(...invetoryItem,pName:e.target.value)}
+    }
+    
+render(){
+    return (
+        <div className="AddIngredientForm">
+            <form>
+                
+
+            </form>
+        </div>
+    )
+}
+
+}
